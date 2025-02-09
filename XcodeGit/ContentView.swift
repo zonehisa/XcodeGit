22a614c36e98a3a26da8ee3955b2d543d02bbaf6
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-09 12:01:15 +0900

    Initial Commit

---
+++ XcodeGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeGit
+//
+//  Created by 佐藤幸久 on 2025/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by 佐藤幸久 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
